20b68180f352dff4e68fd172d9f725e0cb3ad278
James Corfe <james.corfe@gmail.com>
2023-02-27 16:24:39 +0000

    added new/create actions

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -8,4 +8,22 @@ class SpaceshipsController < ApplicationController
     @spaceship = Spaceship.find(params[:id])
   end
 
+  def new
+    @spaceship = Spaceship.new(params[:id])
+  end
+
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    if @spaceship.save
+      redirect_to spaceship_path(@spaceship)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :description, :capacity, :price, :speed)
+  end
 end
