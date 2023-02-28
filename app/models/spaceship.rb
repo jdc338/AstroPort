2016efbef6cd28539e4fe64aed18fb3ec8797269
Oliver Chesney <ojechesney@yahoo.co.uk>
2023-02-28 12:10:10 +0000

    "Added proper design to cards, faker seed works"

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,6 @@
 class Spaceship < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  # has_many :reviews, dependent: :destroy
 
   validates :name, :description, :capacity, :price, :speed, presence: true
   validates :capacity, :price, :speed, numericality: true
