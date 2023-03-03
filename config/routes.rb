c9781381ade802c4d92674b570d620ae5b3abf34
James Corfe <james.corfe@gmail.com>
2023-03-03 12:36:36 +0000

    reviews function done, frontend needed..

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :spaceships do
-    resources :reviews
-    resources :bookings, except: :index
+    resources :bookings, except: :index do
+      resources :reviews
+    end
   end
 
   resources :bookings, only: :index
   get "myspaceships", to: "spaceships#myindex"
+  get "bookings", to: "reviews#new"
 end
